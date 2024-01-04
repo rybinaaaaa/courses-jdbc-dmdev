f806110528f36d499c2ed1b77f11721327bfbe72
rybinali <rybinaaaaa.a@gmail.com>
2024-01-04 20:35:28 +0100

    add execute update

--- src/main/java/org/rybina/DriverRunner.java
+++ src/main/java/org/rybina/DriverRunner.java
@@ -14,8 +14,14 @@ public class DriverRunner {
         Class<Driver> driverClass = Driver.class;
 
         //language=PostgreSQL
-        String sql = """
+        String sql1 = """
                 SELECT * FROM AIRCRAFT
+                """;
+
+        //language=PostgreSQL
+        String sql2 = """
+                insert into aircraft (model)
+                values ('TEST 001')
                 """;
 
         try(Connection con = ConnectionManager.open();
@@ -23,10 +29,13 @@ public class DriverRunner {
         ) {
             System.out.println(con.getTransactionIsolation());
 
-//            method execute используется для ddl операций
+//            method execute преймущественно используется для ddl операций, но вообще его можно использовать повсеместно
 
-            Boolean result = statement.execute(sql);
-            System.out.println(result);
+            Boolean result1 = statement.execute(sql1);
+            System.out.println(result1);
+
+//            method executeUpdate используется для dml запросов, !КОТОРЫЕ НИЧЕГО НЕ ВОЗВРАЩАЮТ!
+            var result = statement.executeUpdate(sql2);
         }
     }
 }
