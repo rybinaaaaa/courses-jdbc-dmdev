05dcbff9ba81a3fdcabe0dd010c096b3fe82be65
rybinali <rybinaaaaa.a@gmail.com>
2024-01-04 20:24:16 +0100

    add first request -- ddl operations

--- src/main/java/org/rybina/DriverRunner.java
+++ src/main/java/org/rybina/DriverRunner.java
@@ -7,12 +7,26 @@ import util.ConnectionManager;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DriverRunner {
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
-        try(Connection con = ConnectionManager.open()) {
+
+        //language=PostgreSQL
+        String sql = """
+                SELECT * FROM AIRCRAFT
+                """;
+
+        try(Connection con = ConnectionManager.open();
+            Statement statement = con.createStatement()
+        ) {
             System.out.println(con.getTransactionIsolation());
+
+//            method execute используется для ddl операций
+
+            Boolean result = statement.execute(sql);
+            System.out.println(result);
         }
     }
 }
